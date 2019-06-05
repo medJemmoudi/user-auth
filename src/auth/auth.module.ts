c85602b2b56315bf733e87c7837d032e330497e5
m.jemmoudi <med.jemmoudi@gmail.com>
2019-06-05 11:12:06 +0200

    feat(auth): adding auth module dependencies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { LocalSignupStrategy } from './strategies/local.signup.strategy';
+import { LocalLoginStrategy } from './strategies/local.login.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+  	UsersModule
+  ],
+  providers: [
+  	AuthService,
+  	LocalSignupStrategy,
+    LocalLoginStrategy,
+  ],
+  exports: [AuthService],
   controllers: [AuthController]
 })
 export class AuthModule {}
